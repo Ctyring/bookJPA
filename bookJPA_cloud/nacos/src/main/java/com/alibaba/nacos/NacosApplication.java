0cbdad218aac23e2af34299b6604bf51a4a0bc2d
Ctyring <75520657+Ctyring@users.noreply.github.com>
2022-06-16 20:48:31 +0800

    完成nacos

---
+++ bookJPA_cloud/nacos/src/main/java/com/alibaba/nacos/NacosApplication.java
@@ -0,0 +1,35 @@
+package com.alibaba.nacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Nacos 启动类
+ * @author cty
+ */
+@SpringBootApplication(scanBasePackages = "com.alibaba.nacos")
+@ServletComponentScan
+@EnableScheduling
+public class NacosApplication {
+
+    /**
+     * 是否单机模式启动
+     */
+    private static String standalone = "true";
+
+    /**
+     * 是否开启鉴权
+     */
+    private static String enabled = "false";
+
+    public static void main(String[] args) {
+        System.setProperty("nacos.standalone", standalone);
+        System.setProperty("nacos.core.auth.enabled", enabled);
+        System.setProperty("server.tomcat.basedir","logs");
+        //自定义启动端口号
+        System.setProperty("server.port","8848");
+        SpringApplication.run(NacosApplication.class, args);
+    }
+}
